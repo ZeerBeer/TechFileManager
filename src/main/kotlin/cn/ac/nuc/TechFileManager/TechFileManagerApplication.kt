9799ce0a765dbb5d3ab76cbb1455f864d809ebe1
zeerbeer <zeerbeer@qq.com>
2021-02-07 11:58:20 +0800

    initialized by start.spring.io

---
+++ src/main/kotlin/cn/ac/nuc/TechFileManager/TechFileManagerApplication.kt
@@ -0,0 +1,11 @@
+package cn.ac.nuc.TechFileManager
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TechFileManagerApplication
+
+fun main(args: Array<String>) {
+	runApplication<TechFileManagerApplication>(*args)
+}
---
+++ src/test/kotlin/cn/ac/nuc/TechFileManager/TechFileManagerApplicationTests.kt
@@ -0,0 +1,13 @@
+package cn.ac.nuc.TechFileManager
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TechFileManagerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
